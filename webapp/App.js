8f858800fda74237dd430eb30009233ea0bf9899
NicholasConnors <nicholasconnors@hotmail.ca>
2019-03-18 21:41:55 -0400

    Revert "Updated webapp"
    This reverts commit 8a9781818b1dc6013f851f3062d66993e8df5e00.

--- webapp/App.js
+++ webapp/App.js
@@ -1,477 +1,232 @@
-canvas = document.getElementById('musicCanvas')
-context = canvas.getContext("2d")
-VERSE_ROWS = 37
-VERSE_COLS = 16
-CELL_PADDING = 2
-CELL_WIDTH = 32
-CELL_HEIGHT = 10
-SONG_LEN = 1
-PROG_BAR_HEIGHT = 8
-SONG_ORDER_CELL_HEIGHT = 32
-SONG_ORDER_CELL_WIDTH = 32
+canvas = document.getElementById('musicCanvas');
+context = canvas.getContext("2d");
+var p = -0.5;
+var rw = 40;
+var rh = 12;
 
-var playing = false
+var cols = 16; //beats
+var rows = 37; //notes
+var lineWidth=4;
 
-scales = {
-	"major" : [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-	"none" : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	"minor" : [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0]	
+var last_time = 0;
+
+var song_menu_height = 0;
+canvas.width = cols * rw;
+canvas.height = (rows + song_menu_height) * rh;
+
+var major_scale = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+var no_scale = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+var minor_scale = [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0]
+
+var hidden_notes = Array.from(Array(rows), (x, index) => major_scale[(index - 1) % 12]);
+
+function new_verse() {
+	var verse = new Object();
+	verse.note_grid = Array.from(Array(rows * cols), () => false); 
+	return verse;
+}
+var current_verse = new_verse();
+var verses = [current_verse];
+
+var prediction = [];
+
+//Drawing grid
+function drawBoard(){
+	// Draw measures
+	context.fillStyle = '#BBB';
+	for(var x = 0; x < cols; x+= 8) {
+		context.fillRect(x * rw, 0, 4 * rw, canvas.height);
+	}
+	context.fillStyle = '#000';
+	
+	// Cover up non-key notes
+	context.fillStyle = '#000';
+	for(var y = 0; y <= rows; y++) {
+		if(hidden_notes[y] == 0) {
+			context.fillRect(0, (rows - y) * rh, canvas.width, rh);
+		}
+	}
+	
+	// Draw root notes
+	context.fillStyle = '#643';
+	for(var y = 0; y <= rows; y+=12) {
+		context.fillRect(0, (rows - y - 1) * rh, canvas.width, rh);
+	}
+	
+	// Draw AI predictions
+	context.fillStyle = '#0E9';
+	for(var i=0; i<prediction.length;i++) {
+		t = prediction[i][0];
+		n = prediction[i][1];
+		context.fillRect(t*rw, (rows - n)*rh, rw, rh);
+	}
+	
+	// Draw notes
+	context.fillStyle='#F38';
+	for(var i=0; i < current_verse.note_grid.length; i++) {
+		if(current_verse.note_grid[i]) {
+			x = (i % cols) * rw;
+			y = (rows - Math.floor(i / cols)) * rh;
+			context.fillRect(x, y, rw, rh);
+		}
+	}
+	
+	// Draw grid
+    for (var x = 0; x <= cols * rw; x += rw) {
+        context.moveTo(0.5 + x + p, p);
+        context.lineTo(0.5 + x + p, ((rows + song_menu_height) * rh) + p);
+    }
+
+    for (var x = 0; x <= (rows + song_menu_height) * rh; x += rh) {
+        context.moveTo(p, 0.5 + x + p);
+        context.lineTo((cols * rw) + p, 0.5 + x + p);
+    }
+	context.lineWidth=lineWidth;
+    context.strokeStyle = "black";
+    context.stroke();
+	
+	//context.fillStyle= '#FFF';
+	//context.fillRect(lineWidth/2, rows*rh + lineWidth/2, canvas.width - lineWidth, rh*song_menu_height - lineWidth);
 }
 
+//Handle mouse
+function click(x, y) {
+	x = Math.floor(x/rw);
+	y = Math.floor(y/rh);
+	if(y >= rows || y < 0 || x >= cols || x < 0) return;
+	
+	y = rows - y; //invert
+	if(x >= last_time) last_time = x + 1;
+	
+	current_verse.note_grid[y * cols + x] = !current_verse.note_grid[y * cols + x];
+}
+
+function redraw() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawBoard();
+	
+}
+
+function refresh() {
+	console.log('clear');
+	for(var i=0; i < current_verse.note_grid.length; i++) {
+		if(current_verse.note_grid[i])
+			current_verse.note_grid[i] = false;
+	}
+	last_time = 0;
+	prediction=[];
+	redraw();
+}
+
+canvas.onmousedown = function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+
+  click(mouseX, mouseY);
+  redraw();
+};
+
+suggest();
+redraw();
+
+//AI
+function suggest() {
+	// PLACEHOLDER
+	t = last_time;
+	num = Math.floor(Math.random() * rows)
+	new_prediction = [num, num + 2, num + 5];
+	
+	prediction = [];
+	for(var i=0; i < new_prediction.length; i++) {
+		prediction.push([t, new_prediction[i]]);
+	}
+	redraw();
+}
+
+function use_suggestion() {
+	for(var i=0; i<prediction.length;i++) {
+		t = prediction[i][0];
+		n = prediction[i][1];
+		
+		current_verse.note_grid[n * cols + t] = true;
+	}
+	last_time++;
+	suggest();
+}
+
+//MUSIC
 function freq(n) {
 	return Math.pow(2, (n-69)/12) * 440;
 }
 
-function song() {
-	return song = {
-		bpm : 120,
-		rootNote: 64,
-		verses : [],
-		order : [],
-		synth : 1,
-		
-		addVerse(verse) {
-			this.verses.push(verse)
-			this.order.push(this.verses.indexOf(verse))
-		},
-		
-		delVerse(verse) {
-			if(this.verses.indexOf(verse) == 0)
-				return false
-			else {
-				var ind = this.verses.indexOf(verse)
-				if(ind == -1) return false
-				for(var i = 0; i < this.order.length; i++) {
-					if(this.order[i] > ind)
-						this.order[i] -= 1
-					else if(this.order[i] == ind) 
-						this.order[i] = 0
-				}
-				this.verses.splice(ind, 1)
-				return true
-			}
-		},
-		
-		chords() {
-			chordArray = []
-
-			for(var i = 0; i < this.order.length; i++) {
-				verse = this.verses[this.order[i]]
-				
-				for(var t=0; t < VERSE_COLS; t++) {
-
-					var notes = []
-					for(var n=0; n <= VERSE_ROWS; n++) {
-						if(verse.notes[n * VERSE_COLS + t]) {
-							notes.push(freq(n + this.rootNote - 12 - 1))
-						}
-					}
-					
-					if(notes.length > 0) {
-						chordArray.push([(t + i * VERSE_COLS)/2, notes])
-					}
-				}
-			}
-			return chordArray
-		}
-	}
+var root_note = 60;
+root_note_selection = document.getElementById("keySelect");
+function root_note_change() {
+	root_note = parseInt(root_note_selection.options[root_note_selection.selectedIndex].value);
+	console.log(root_note);
 }
 
-function newVerse() {
-	return verse = {
-		notes : Array.from(Array(VERSE_ROWS * VERSE_COLS), () => false),
-		clear() {
-			this.notes = Array.from(Array(VERSE_ROWS * VERSE_COLS), () => false)
-		}
-	}
-}
-function cloneVerse(old_verse) {
-	var clone_verse = newVerse()
-	clone_verse.notes = old_verse.notes.slice(0)
-	return clone_verse
-}
-
-function musicGrid() {
-	return musicGrid = {	
-		
-		width : VERSE_COLS * CELL_WIDTH + CELL_PADDING,
-		height : VERSE_ROWS * CELL_HEIGHT + CELL_PADDING,
-		scale : scales["major"],
-		
-		draw() {
-			// Black background
-			context.fillStyle = '#000';
-			context.fillRect(0, 0, this.width, this.height);
-			
-			// Draw notes
-			for(var x = 0; x < VERSE_COLS; x++) {
-				for(var y = 0; y < VERSE_ROWS; y++) {
-					if(currentVerse && currentVerse.notes[y * VERSE_COLS + x]) {
-						// Active note
-						context.fillStyle = '#F79';
-					}
-					else if(this.scale[(y - 0) % 12] == 0) {
-						//Nothing
-						continue
-					}
-					else if(y % 12 == 0) {
-						// Root note
-						context.fillStyle = '#756'
-					}
-					else if(Math.round((x + 2) / 4) % 2 == 0) {
-						// Inactive note
-						context.fillStyle = '#CCC';
-					}
-					else {
-						// Inactive note
-						context.fillStyle = '#999';
-					}
-					context.fillRect(
-						x * CELL_WIDTH + CELL_PADDING,
-						(VERSE_ROWS - y - 1) * CELL_HEIGHT + CELL_PADDING,
-						CELL_WIDTH - 2 * CELL_PADDING,
-						CELL_HEIGHT - 2 * CELL_PADDING
-					)
-				}
-			}
-			
-			// VERSE ORDER
-			context.fillStyle = '#000';
-			context.fillRect(0, musicGrid.height + PROG_BAR_HEIGHT, canvas.width, SONG_ORDER_CELL_HEIGHT);
-			
-			context.font = "12px Arial"
-			for(var i = 0; i < song.order.length; i++) {
-				context.fillStyle = '#999'
-				context.fillRect(
-					i * SONG_ORDER_CELL_WIDTH + CELL_PADDING,
-					musicGrid.height + PROG_BAR_HEIGHT + CELL_PADDING,
-					SONG_ORDER_CELL_WIDTH - 2 * CELL_PADDING,
-					SONG_ORDER_CELL_HEIGHT - 2 * CELL_PADDING
-				)
-				context.fillStyle = '#FFF'
-				context.fillText(
-					"S" + song.order[i],
-					i * SONG_ORDER_CELL_WIDTH + (SONG_ORDER_CELL_WIDTH - 20) / 2,
-					musicGrid.height + PROG_BAR_HEIGHT + (SONG_ORDER_CELL_HEIGHT + 2) / 2
-				)
-			}
-		},
-		
-		setVerse(verse) {
-			this.currentVerse = verse
-		}
-	}
-}
-
-// CANVAS LISTENER
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-canvas.onmousedown = function(e){
-	if(playing) return
+scale_selection = document.getElementById("scaleSelect");
+function scale_change() {
+	scale_choice = scale_selection.options[scale_selection.selectedIndex].value;
+	console.log(scale_choice);
+	selection = no_scale;
+	if(scale_choice == "major") selection = major_scale;
+	if(scale_choice == "minor") selection = minor_scale;
 	
-	var mouseX = e.pageX - this.offsetLeft
-	var mouseY = e.pageY - this.offsetTop
-	
-	if(mouseY < musicGrid.height) {
-		var x = Math.floor((e.pageX - this.offsetLeft) / CELL_WIDTH)
-		// Y is inverted
-		var y = VERSE_ROWS - Math.floor((e.pageY - this.offsetTop) / CELL_HEIGHT) - 1
-		
-		// Check in bounds
-		if(!(y >= VERSE_ROWS || y < 0 || x >= VERSE_COLS || x < 0) && currentVerse && musicGrid) {
-			currentVerse.notes[y * VERSE_COLS + x] = !currentVerse.notes[y * VERSE_COLS + x]
-			musicGrid.draw()
-		}
-	} else if(mouseY < canvas.height && mouseY > canvas.height - SONG_ORDER_CELL_HEIGHT) {
-		var orderInd = Math.floor((mouseX) / SONG_ORDER_CELL_WIDTH)
-		if(orderInd >= 0 && orderInd < song.order.length) {
-			song.order[orderInd] = song.order[orderInd] + 1 >= song.verses.length ? 0 : song.order[orderInd] + 1
-			musicGrid.draw()
-		}
-	}
+	hidden_notes = Array.from(Array(rows), (x, index) => selection[(index - 1) % 12]);
+	redraw();
 }
 
-// MUSIC
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-var chordPart = null
-function musicHandler() {
-	return musicHandler = {
-		_instrument : null,
+//Time value then chord array
+function makeChordArray() {
+	var chordArray = [];
+	for(var t=0; t < cols; t++) {
 		
-		instrument(choice) {
-			if(this._instrument)
-				this._instrument.dispose
-			switch(choice) {
-				case 1:
-					this._instrument = new Tone.PolySynth(4, Tone.Synth, {
-						"volume" : -8,
-						oscillator  : {
-							type  : "sine"
-						}  ,
-						envelope  : {
-							attack  : 0.005 ,
-							decay  : 0.1 ,
-							sustain  : 0.3 ,
-							release  : 1
-						}
-					}).toMaster()
-					break
-				case 2:
-					this._instrument = new Tone.PolySynth(4, Tone.Synth, {
-						"volume" : -8,
-						oscillator  : {
-							type  : "triangle"
-						}  ,
-						envelope  : {
-							attack  : 0.005 ,
-							decay  : 0.1 ,
-							sustain  : 0.3 ,
-							release  : 1
-						}
-					}).toMaster()
-					break
-					break
-				case 3:
-					this._instrument = new Tone.PolySynth(4, Tone.Synth, {
-						"volume" : -8,
-						oscillator  : {
-							type  : "square"
-						}  ,
-						envelope  : {
-							attack  : 0.005 ,
-							decay  : 0.1 ,
-							sustain  : 0.3 ,
-							release  : 1
-						}
-					}).toMaster()
-					break
-				case 4:
-				default:
-					this._instrument = new Tone.PolySynth(4, Tone.FMSynth, {
-						"volume" : -8,
-						"modulationIndex" : 12.22,
-						"envelope" : {
-						"attack" : 0.01,
-						"decay" : 0.2
-						},
-						"modulation" : {
-						"type" : "square"
-						},
-						"modulationEnvelope" : {
-						"attack" : 0.2,
-						"decay" : 0.01
-						}
-					}).toMaster()
-					break
-			}
-			return this._instrument
-		},
-		
-		play(song) {		
-			instrument = this.instrument(song.synth)
-			console.log(song.synth)
-			
-			Tone.Transport.bpm.value = 120
-		
-			chordPart = new Tone.Part(function(time, chord){
-				instrument.triggerAttackRelease(chord, '4n', time);
-			}, song.chords() );
-			
-			chordPart.loop = true;
-			chordPart.loopEnd = '' + (4 * song.order.length) + 'm'
-			chordPart.start(0)
-			
-			Tone.Transport.bpm.value = song.bpm
-			Tone.Transport.start("+0.1")
-		},
-		
-		stop() {
-			Tone.Transport.stop(0)
-			Tone.Transport.clear(chordPart)
-			if(chordPart) {
-				chordPart.dispose()
-				chordPart = null	
+		var notes = [];
+		for(var i=0; i < verses.length; i++) {
+			for(var n=0; n <= rows; n++) {
+				if(verses[i].note_grid[n * cols + t]) {
+					notes.push(freq(n + root_note - 12 - 1))
+				}
 			}
 		}
+		
+		if(notes.length > 0) {
+			chordArray.push([t/4, notes]);
+		}
 	}
+	console.log(chordArray);
+	return chordArray;
 }
 
-// BUTTONS
-document.getElementById('stop').disabled = true
+var piano;
 function play() {
-	if(song && musicHandler) {
-		if(playing)
-			stop()
-		
-		playing = true;
-		musicHandler.play(song)
-		
-		document.getElementById('play').disabled = true
-		document.getElementById('stop').disabled = false
-		document.getElementById('selectRoot').disabled = true
-		document.getElementById('addVerse').disabled = true
-		document.getElementById('clearVerse').disabled = true
-		document.getElementById('delVerse').disabled = true	
-		document.getElementById('delSequence').disabled = true	
-		document.getElementById('addSequence').disabled = true	
-		
-		progressBar()
-	}
+	stop();
+	piano = new Tone.PolySynth(4, Tone.Synth, {
+		"volume" : -8,
+		"oscillator" : {
+			"partials" : [1, 2, 5],
+		},
+		"portamento" : 0.005
+	}).toMaster()
+	
+    var myChords = makeChordArray();
+
+	var chordPart = new Tone.Part(function(time, chord){
+		piano.triggerAttackRelease(chord, '4n', time);
+	}, myChords ).start(0);
+	
+	Tone.Transport.timeSignature = 4;
+	Tone.Transport.bpm.value = 120;   
+	Tone.Transport.start("+0.1");
 }
+
 function stop() {
-	if(musicHandler) {
-		playing = false
-		musicHandler.stop()
-		
-		document.getElementById('play').disabled = false
-		document.getElementById('stop').disabled = true
-		document.getElementById('selectRoot').disabled = false
-		document.getElementById('addVerse').disabled = false
-		document.getElementById('clearVerse').disabled = false
-		document.getElementById('delVerse').disabled = false	
-		document.getElementById('delSequence').disabled = false	
-		document.getElementById('addSequence').disabled = false	
-		
-		// Hide progress bar
-		context.fillStyle = '#000';
-		context.fillRect(0, musicGrid.height, canvas.width, PROG_BAR_HEIGHT);
-		context.fillRect(0, canvas.height - PROG_BAR_HEIGHT, canvas.width, PROG_BAR_HEIGHT);
+	Tone.Transport.stop();
+	Tone.Transport.cancel(0);
+	if(piano) {
+	   piano.dispose();
+	   piano = null;
 	}
 }
-function enable() {
-	document.getElementById('bpm')
-}
-
-document.getElementById('bpm').addEventListener('input', e => {
-  if(song)
-	song.bpm = e.target.value
-  Tone.Transport.bpm.value = e.target.value
-  document.getElementById("bpmText").textContent=e.target.value;
-})
-
-function clearVerse() {
-	if(currentVerse && musicGrid && !playing) {
-		currentVerse.clear()
-		musicGrid.draw()
-	}
-}
-verseDropdown = document.getElementById("selectVerse");
-function delVerse() {
-	if(currentVerse && song && !playing) {
-		ind = song.verses.indexOf(currentVerse)
-		if(song.delVerse(currentVerse)) {			
-			verseDropdown.remove(verseDropdown.options.length-1)
-			setVerse(0)
-		}
-		musicGrid.draw()
-	}
-}
-function addVerse() {
-	if(song && !playing) {
-		var new_verse = newVerse()
-		//if(currentVerse)
-		//	new_verse = cloneVerse(currentVerse)
-		//else
-		//	new_verse = newVerse()
-		song.addVerse(new_verse)
-		currentVerse = new_verse
-		
-		var opt = document.createElement("OPTION")
-		opt.text = song.verses.indexOf(currentVerse)
-		opt.value = song.verses.indexOf(currentVerse)
-		verseDropdown.add(opt)
-		
-		verseDropdown.selectedIndex = song.verses.indexOf(currentVerse)
-	
-		musicGrid.draw()
-	}
-}
-function selectVerse() {
-	if(musicGrid) {
-		verseChoice = verseDropdown.options[verseDropdown.selectedIndex].value;
-		currentVerse = song.verses[verseChoice]
-		musicGrid.draw()	
-	}
-}
-function setVerse(i) {
-	currentVerse = song.verses[i]
-	verseDropdown.selectedIndex = i
-	musicGrid.draw()
-}
-function changeRoot() {
-	dropdown = document.getElementById("selectRoot");
-	if(song)
-		song.rootNote = parseInt(dropdown.options[dropdown.selectedIndex].value);
-}
-function changeScale() {
-	dropdown = document.getElementById("selectScale");
-	if(musicGrid) {
-		musicGrid.scale = scales[dropdown.options[dropdown.selectedIndex].value];
-		musicGrid.draw()
-	}
-}
-function changeSynth() {
-	dropdown = document.getElementById("selectSynth");
-	if(song) {
-		song.synth = parseInt(dropdown.options[dropdown.selectedIndex].value);
-	}
-}
-function addSequence() {
-	song.order.push(0)
-	musicGrid.draw()
-}
-function delSequence() {
-	if(song.order.length != 1)
-		song.order.splice(song.order.length - 1, 1)
-	musicGrid.draw()
-}
-
-// PROGRESS BAR
-function progressBar() {
-	if(chordPart && playing) {
-		progress = chordPart.progress
-		
-		var playingVerseIndex = Math.floor(progress * song.order.length)
-		if(currentVerse != song.verses[song.order[playingVerseIndex]])
-			setVerse(song.order[playingVerseIndex])
-		
-		var playingVerseProgress = (progress * song.order.length) % 1
-	
-		context.fillStyle = '#000';
-		context.fillRect(0, musicGrid.height, canvas.width, PROG_BAR_HEIGHT);
-		context.fillRect(0, canvas.height - PROG_BAR_HEIGHT, canvas.width, PROG_BAR_HEIGHT);
-		
-		// Draw play line
-		context.fillStyle = '#F49';
-		context.fillRect(
-			CELL_PADDING, 
-			musicGrid.height + CELL_PADDING, 
-			playingVerseProgress * (canvas.width - 2 * CELL_PADDING), 
-			PROG_BAR_HEIGHT - 2 * CELL_PADDING
-		)
-		context.fillRect(
-			CELL_PADDING, 
-			canvas.height - PROG_BAR_HEIGHT + CELL_PADDING, 
-			progress * (song.order.length * SONG_ORDER_CELL_WIDTH - 2 * CELL_PADDING), 
-			PROG_BAR_HEIGHT - 2 * CELL_PADDING
-		)
-		
-		setTimeout(progressBar, 100);
-	}
-}
-
-// RUN
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-var musicGrid = musicGrid()
-canvas.width = musicGrid.width
-canvas.height = musicGrid.height + PROG_BAR_HEIGHT + SONG_ORDER_CELL_HEIGHT + PROG_BAR_HEIGHT
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
-var song = song()
-var currentVerse = null;
-addVerse()
-
-musicGrid.draw()
-
-var musicHandler = musicHandler()
